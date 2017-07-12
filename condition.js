05a84646088be44b1c1497e74ceff151debffd6d
jhaskellASC4 <jordan.haskell@meca-nyc.org>
2017-07-12 11:20:41 -0400

    added new flies

---
+++ condition.js
@@ -0,0 +1,12 @@
+I
+// Declare and array with two Cool Nick entries to increase his odds
+var people = ["Chris", "Will", "Cool Nick", "Edgae", "Faisal", "Cool Nick" ];
+
+// Function to get random number
+function getRandonWinner(index) {
+    // var randomIndex = Math.random() * index;
+    return Math.floor(Math.floor() *index);
+}
+
+// Prints out a random name based to the function
+console.log(people[getRandonWinner(people.lenghr)]);
---
+++ pokemonVariable.js
@@ -0,0 +1,10 @@
+squirleHP - 44;
+squirleNum - 7;
+squirleType - "Water";
+squirleBestMove - "Bite";
+squirleLastEvo - Blastoise
+squirleAttack - 48;
+squirleDefense - 65;
+squirleBaseEXP - 66;
+squirleEggCycles - 20;
+squirleCatchRate - 45;
